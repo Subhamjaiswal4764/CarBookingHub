d8c33b708e09cc819057c9351d6f65982a93dfd9
Shubham Jaiswal <subhamkumar.sk101@gmail.com>
2022-10-07 16:55:15 +0530

    changes checkbox

--- force-app/main/default/lwc/carFilter/carFilter.js
+++ force-app/main/default/lwc/carFilter/carFilter.js
@@ -9,15 +9,15 @@ import CAR_OBJECT from '@salesforce/schema/Car__c';
 import CAR_CATEGORY  from '@salesforce/schema/Car__c.Category__c';
 import CAR_MAKE from '@salesforce/schema/Car__c.Make__c';
 
-// const CAR_MAKE_ERROR='error loading car make type';
-// const CAR_CATEGORY_ERROR='error loading car category type';
+const CAR_MAKE_ERROR='error loading car make type';
+const CAR_CATEGORY_ERROR='error loading car category type';
 export default class CarFilter extends LightningElement {
      
-    carM
-    carC
     
-    // car_make_error_msg=CAR_MAKE_ERROR
-    // car_category_error_msg=CAR_CATEGORY_ERROR
+    timer
+    
+    car_make_error_msg=CAR_MAKE_ERROR
+    car_category_error_msg=CAR_CATEGORY_ERROR
     filters={
         searchKey:'',
         maxPrice:99999999
@@ -52,44 +52,62 @@ export default class CarFilter extends LightningElement {
         console.log(this.filters);
         this.sendDataToCarTileList();
     }
-    handleCarMakeRadio(event)
-    {
-        const{name,value}=event.target.dataset;
-        console.log("name"+name);
-        console.log("value"+value);
-      
-        
-    }
-    sendDataToCarTileList(){
-        publish(this.messageContextProperty, CARS_FILTERED_MSG_CHANNEL, {
-            filteredData: this.filters
-        })
-    }
-    
-    
- 
-
-
-
-
-
-
-
-
-    
-    /**
-     * for printing the car make type 
-     */
-    //  @wire(getPicklistValues, { recordTypeId: 'CarInfo.data.defaultRecordTypeId', fieldApiName: CAR_MAKE })
-    // propertyOrFunction({data,error}){
-    //     if(data){
-    //         this.car_m=data;
-    //         console.log('car_category'+this.car_category);
-    //     }
-    //     if(error){
-    //         console.error(car_make_error_msg);
-    //     }
+    // handleCarMakeRadio(event)
+    // {
+    //     // if(!this.filters.carC){
+    //     //     console.log("in the first if")
+    //     //     const carC=this.carC.data.values.map(item=>item.value)
+    //     //     const carM=this.carM.data.values.map(item=>item.value)
+    //     //     this.filters={...this.filters, carC, carM}
+    //     //     // this.filters={...this.filters,carM:carM}
+    //     // }
+    //     console.log('when itis not tehir carc');
+    //     console.log(this.filters)
+    //     const{name,value}=event.target.dataset;
+    //     console.log("name"+name);
+    //     console.log("value"+value);
+    //   if(event.target.checked){
+    //     console.log("i am checked");
+    //     if(!this.filter[name].includes(value)){
+    //     this.filters[name] = [...this.filters[name], value]
+    //     console.log('if');
+    //     console.log(this.filters)
+    //   }
     // }
+    //   else{
+    //     this.filters[name]= this.filters[name].filter(item=>item !== value)
+    //     console.log('else');
+    //     console.log(this.filters)
+    //   }
+    //     this.sendDataToCarTileList();
+    // }
+    handleCarMakeRadio(event){
+        if(!this.filters.carC){
+            const carC = this.carC.data.values.map(item=>item.value)
+            const carM = this.carM.data.values.map(item=>item.value)
+            this.filters = {...this.filters, carC, carM}
+        }
+        const {name, value} = event.target.dataset
+        // console.log("name", name)
+        // console.log("value", value)
+        if(event.target.checked){
+            if(!this.filters[name].includes(value)){
+                this.filters[name] = [...this.filters[name], value]
+            }
+        } else {
+            this.filters[name] =  this.filters[name].filter(item=>item !==value)
+        }
+        this.sendDataToCarList()
+    }
 
-
+    sendDataToCarTileList(){
+        window.clearTimeOut(this.timer);
+        this.timer=window.setTimeout(() => {
+            publish(this.messageContextProperty, CARS_FILTERED_MSG_CHANNEL, {
+                filteredData: this.filters
+            })  
+        }, 400);
+       
+    }
+ 
 }
